1be41e28b39fef7510dc12ab4a2e2d7ec9af2d00
loominem <37760868+loominem@users.noreply.github.com>
2019-04-11 23:16:32 +0800

    batch -v1.1

--- PAT.Lib.Batch.cs
+++ PAT.Lib.Batch.cs
@@ -8,16 +8,15 @@ using PAT.Common.Classes.Expressions.ExpressionClass;
 //the namespace must be PAT.Lib, the class and method names can be arbitrary
 namespace PAT.Lib
 {
-	    public class Batch : ExpressionValue
+	public class Batch : ExpressionValue
     {
-    	public int batchID;
+        private int batchID;
         public bool isValid;
     	public int fridgeID;
     	public List<int[]> vials = new List<int[]>(3);
     	public DateTime expiryDate;
-    	
-    	public Batch(){}
-    	
+    	public IList<Vial> batch;
+    	public Batch() {batch = new List<Vial>();}
     	public Batch(int batchID, bool isValid, int fridgeID, DateTime expiryDate){
     		batchID = batchID;
     		isValid = isValid;
@@ -28,6 +27,11 @@ namespace PAT.Lib
 /*        public Batch(Batch batch):this(batch.batchID, batch.isValid, batch.fridgeID,batch.expiryDate){
        } */
     	
+        public void addVial(Vial vial){
+            batch.Add(vial);
+        }
+
+
        public void addBatch(int batchID, DateTime expiryDate){
         	if(vials == null) 
         	{
@@ -81,17 +85,20 @@ namespace PAT.Lib
             get {return batchID+""; }
         }
 
+        public int BatchID { get => batchID; set => batchID = value; }
+        
     }
     
     
     public class Vial: ExpressionValue {
-    	public IList<Vial> Batch;
+    	
     	
     	//default constructor
     	public Vial() { Vial = new List<Batch>(); }
-    	public void Add(Batch batch){
-    		Vial.Add(batch);
-    	}
+
+        public void useVial(Vial vial){
+            batch.
+        }
     	public Batch GetBlock(int index){
     		return Vial[index];
     	}
